68667b87c882d2e5f73009ef00062fb69517389a
Richard Waid <richard@iopen.net>
2003-08-22 05:44:04 +0000

    Massive changes, in order to support mapping one property on top of another, callable properties and more.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/CustomProperties/trunk%4050

--- CustomProperties.py
+++ CustomProperties.py
@@ -6,16 +6,24 @@
 # You MUST follow the rules in README_STYLE before checking in code
 # to the head. Code which does not follow the rules will be rejected.  
 #
+# This file also contains code used directly or modified from original Zope
+# sources, which are covered by the Zope Public Licence Version 2.0. 
+#
+from AccessControl import getSecurityManager, ClassSecurityInfo
+
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from Globals import InitializeClass, PersistentMapping
 from OFS.SimpleItem import SimpleItem
 from OFS.PropertyManager import PropertyManager
+from cgi import escape
 
 class CustomProperties(SimpleItem, PropertyManager):
     """ A simple properties object for creating just a property sheet.
     
     """
-    version = 0.9
+    security = ClassSecurityInfo()
+    
+    version = 0.98
     
     meta_type = "Custom Properties"
     manage_options = PropertyManager.manage_options + \
@@ -25,8 +33,86 @@ class CustomProperties(SimpleItem, PropertyManager):
         """ Initialize the properties.
            
         """
+        self._property_mapping = {}
+        self._p_changed = 1
+        
         self.id = id
         self.title = title
+
+    def __setattr__(self, name, value):
+        d = self.__dict__
+        # we need to avoid the catch 22 of the property mapping not existing, since
+        # we may actually be creating the property mapping
+        pm = d.get('_property_mapping', {})
+        d[name] = value
+        
+        mp = pm.get(name, None)
+        if mp and mp != name:
+            setattr(self, mp, value)
+
+    def __getattr__(self, name):
+        d = self.__dict__
+        
+        pm = d.get('_property_mapping', {})
+        
+        try:
+            if pm.has_key(name):
+                return self.__dict__[pm[name]]
+            
+            return self.__dict__[name]
+        except:
+            raise AttributeError, escape(name)
+
+    def getProperty(self, id, d=None):
+        """ Get the property 'id', returning the optional second
+            argument or None if no such property is found.
+    
+            Additionally checks to see if there is a mapping between our
+            property and another attribute, and if so, gets that attribute 
+            instead.
+            
+        """
+        if self.hasProperty(id):
+            if self._property_mapping.has_key(id):
+                p = getattr(self, self._property_mapping[id])
+            else:
+                p = getattr(self, id)
+            if callable(p):
+                p = p()
+                
+            return p
+        
+        return d
+        
+    def propertyValues(self):
+        """ Return a list of actual property objects.
+            
+            Modified from the original version to get the property with the
+            getProperty method, to enable overriding there.
+             
+        """
+        return map(lambda i,s=self: s.getProperty(i['id']), self._properties)
+ 
+    def propertyItems(self):
+        """ Return a list of (id,property) tuples.
+        
+            Modified from the original version to get the property with the
+            getProperty method, to enable overriding there.
+            
+        """
+        return map(lambda i,s=self: (i['id'], s.getProperty(i['id'])),
+                                    self._properties)
+        
+    def valid_property_id(self, id):
+        """ A modification of the PropertyManager version. We actually
+            sometimes _need_ to have an existing property, to handle
+            callable properties for instance.
+            
+        """
+        if not id or id[:1]=='_' or (id[:3]=='aq_') \
+           or (' ' in id) or escape(id) != id:
+            return 0
+        return 1
 
     def _updateProperty(self, id, value):
         """ Hook for updating a particular property.
@@ -39,12 +125,124 @@ class CustomProperties(SimpleItem, PropertyManager):
             value = check_method(value)
         # use the regular property sheets storage
         PropertyManager._updateProperty(self, id, value)
-
+        
         # run a reindex if we are CatalogAware
         if hasattr(self, 'reindex_object'):
             self.reindex_object()
+        
+    def _setPropValue(self, id, value):
+        """ Overrides the PropertyManager method of the same name.
+        
+            If the property concerned is callable, it tries to set it with
+            set_NameOfProperty(value).
+            
+            Note: no exception is thrown if we can't set the property with
+            set_NameOfProperty.
+            
+        """
+        self._wrapperCheck(value)
+        
+        if self._property_mapping.has_key(id):
+            id = self._property_mapping[id]
+        # check to see if the property already exists, and if it does, check
+        # our callable hook
+        p = getattr(self, id, None)
+        if callable(p):
+            if hasattr(self, 'set_%s' % id):
+                getattr(self, 'set_%s' % id)(value)
+        else:
+            setattr(self,id,value)
+        
+    def _delPropValue(self, id):
+        """ Overrides the PropertyManager method of the same name.
+        
+            If the property is callable, it tries to delete it with del_NameOfProperty().
+            
+            Note: no exception is thrown if we can't delete the property with
+            del_NameOfProperty.
+            
+        """
+        if self._property_mapping.has_key(id):
+            id = self._property_mapping[id]
+        
+        p = getattr(self, id)
+        
+        if callable(p):
+            if hasattr(self, 'del_%s' % id):
+                getattr(self, 'del_%s' % id)()
+                return
+                
+        delattr(self, id)
 
-    # an example of using foo -- DOCUMENT ME PROPERLY!
+    def _delProperty(self, id):
+        """ Extends the PropertyManager method of the same name.
+        
+        """
+        PropertyManager._delProperty(self, id)
+        if self._property_mapping.has_key(id):
+            self._p_changed = 1
+            mp = self._property_mapping[id]
+            del(self._property_mapping[id])
+
+    def manage_changePropertyMode(self, id, mode='wd'):
+        """ Change the mode of the property. Mode is a string consisting
+            of one or more of 'w','d','r' (writeable, deletable, readable).
+        
+        """
+        new_properties = []
+        if self.hasProperty(id):
+            for property_dict in self._properties:
+                if property_dict['id'] == id:
+                    property_dict['mode'] = mode
+                new_properties.append(property_dict)
+            self._properties = tuple(new_properties)
+        else:
+            raise 'AttributeError', 'No such property %s' % id
+        
+    def manage_addPropertyMapping(self, mapping):
+        """ Adds a mapping between one or more properties and an attribute/method.
+        
+            The property being mapped must exist prior to mapping, as must the
+            attribute/method we are mapping to.
+            
+            Mapping is a dictionary containing {'propertyid': 'attributeid'}.
+            
+        """
+        self._p_changed = 1
+        
+        for id in mapping:
+            if self.hasProperty(id):
+                if hasattr(self, mapping[id]):
+                    self._property_mapping[id] = mapping[id]
+                    # we delete the original attribute, because we are now mapping it,
+                    # and we want getattr to work
+                    delattr(self, id)
+                else:
+                    raise 'AttributeError', 'No such attribute or method %s' % mapping[id]
+            else:
+                raise 'AttributeError', 'No such property %s' % id
+
+    security.declareProtected('Upgrade objects', 'upgrade')
+    security.setPermissionDefault('Upgrade objects', ('Manager', 'Owner'))
+    def upgrade(self):
+        """ Upgrade to the latest version.
+            
+            UnitTest: TestXWFFileLibrary
+        """
+        currversion = getattr(self, '_version', 0)
+        if currversion == self.version:
+            return 'already running latest version (%s)' % currversion
+
+        self._version = self.version
+        
+        self._property_mapping = getattr(self, '_property_mapping', {})
+        self._p_changed = 1
+        
+        return 'upgraded %s to version %s from version %s' % (self.getId(),
+                                                              self._version,
+                                                              currversion)
+                                                              
+    # an example of using value checking -- DOCUMENT ME PROPERLY!
     #def check_foo(self, value):
     #    """ Check the value for foo """
     #    return 'foobar'
--- tests/testCustomProperties.py
+++ tests/testCustomProperties.py
@@ -14,19 +14,104 @@ from Testing import ZopeTestCase
 
 ZopeTestCase.installProduct('CustomProperties')
 
-from Products.CustomProperties import CustomProperties
+from Products.CustomProperties.CustomProperties import CustomProperties
 class TestCustomProperties(ZopeTestCase.ZopeTestCase):
     def afterSetUp(self):
-        pass
+        self.folder._setObject('CustomProperties', 
+                               CustomProperties('CustomProperties','Custom Properties'))
+        
+        self.cp = getattr(self.folder, 'CustomProperties', None)
         
     def afterClear(self):
         pass    
 
+    def test_01_exists(self):
+        self.failUnless(self.cp)
+
+    def test_02_addProperty(self):
+        self.cp.manage_addProperty('dc:Title', 'Hello', 'string')
+        self.assertEqual(self.cp.getProperty('dc:Title'), 'Hello')
+        
+    def test_03_delProperty(self):
+        self.cp.manage_addProperty('dc:Title', 'Hello', 'string')
+        self.cp.manage_delProperties(['dc:Title'])
+        self.failIf(self.cp.hasProperty('dc:Title'))
+
+    def test_04_addCallableProperty(self):
+        setattr(self.cp, 'dc:Description', lambda: 'Hello')
+        self.cp.manage_addProperty('dc:Description', '', 'string')
+        self.assertEqual(self.cp.getProperty('dc:Description'), 'Hello')
+
+    def test_05_delCallableProperty(self):
+        setattr(self.cp, 'dc:Description', lambda: 'Hello')
+        self.cp.manage_addProperty('dc:Description', '', 'string')
+        self.cp.manage_delProperties(['dc:Description'])
+        self.failIf(self.cp.hasProperty('dc:Description'))
+        
+    def test_06_addMappedProperty(self):
+        setattr(self.cp, 'content_type', 'application/pdf')
+        self.cp.manage_addProperty('dc:Format', '', 'string')
+        self.cp.manage_addPropertyMapping({'dc:Format': 'content_type'})
+        self.assertEqual(self.cp.getProperty('dc:Format'), 'application/pdf')
+   
+    def test_07_delMappedCallableProperty(self):
+        setattr(self.cp, 'content_type', lambda: 'application/pdf')
+        self.cp.manage_addProperty('dc:Format', '', 'string')
+        self.cp.manage_addPropertyMapping({'dc:Format': 'content_type'})
+        self.cp.manage_delProperties(['dc:Format'])
+        self.failIf(self.cp.hasProperty('dc:Format'))
+        self.failIf(self.cp._property_mapping.has_key('dc:Format'))
+
+    def test_08_addMappedCallableProperty(self):
+        setattr(self.cp, 'content_type', lambda: 'application/pdf')
+        self.cp.manage_addProperty('dc:Format', '', 'string')
+        self.cp.manage_addPropertyMapping({'dc:Format': 'content_type'})
+        self.assertEqual(self.cp.getProperty('dc:Format'), 'application/pdf')
+        
+    def test_09_delMappedCallableProperty(self):
+        setattr(self.cp, 'content_type', lambda: 'application/pdf')
+        self.cp.manage_addProperty('dc:Format', '', 'string')
+        self.cp.manage_addPropertyMapping({'dc:Format': 'content_type'})
+        self.cp.manage_delProperties(['dc:Format'])
+        self.failIf(self.cp.hasProperty('dc:Format'))
+        self.failIf(self.cp._property_mapping.has_key('dc:Format'))
+
+    def test_10_changePropertyMode(self):
+        setattr(self.cp, 'content_type', lambda: 'application/pdf')
+        self.cp.manage_addProperty('dc:Format', '', 'string')
+        self.cp.manage_addPropertyMapping({'dc:Format': 'content_type'})
+        self.cp.manage_changePropertyMode('dc:Format', 'r')
+        try:
+            apply(self.cp.manage_changeProperties, (), {'dc:Format': 'foo'})
+            self.fail('Should have raised a "BadRequest"')
+        except 'BadRequest':
+            pass
+        self.cp.manage_changePropertyMode('dc:Format', 'w')
+        try:
+            apply(self.cp.manage_changeProperties, (), {'dc:Format': 'foo'})
+        except:
+            self.fail('Should not have raised an Exception')
+
+    def test_11_directSettingOfMappedProperty(self):
+        setattr(self.cp, 'content_type', 'application/pdf')
+        self.cp.manage_addProperty('dc:Format', '', 'string')
+        self.cp.manage_addPropertyMapping({'dc:Format': 'content_type'})
+        
+        self.assertEqual(getattr(self.cp, 'dc:Format', None), 'application/pdf')
+        
+        setattr(self.cp, 'content_type', 'application/wibble')
+        self.assertEqual(getattr(self.cp, 'dc:Format', None), 'application/wibble')
+        self.assertEqual(getattr(self.cp, 'content_type', None), 'application/wibble')
+        setattr(self.cp, 'dc:Format', 'application/pdf')
+        self.assertEqual(getattr(self.cp, 'dc:Format', None), 'application/pdf')
+        self.assertEqual(getattr(self.cp, 'content_type', None), 'application/pdf')
+        
+    
 if __name__ == '__main__':
     framework(descriptions=1, verbosity=1)
 else:
     import unittest
     def test_suite():
         suite = unittest.TestSuite()
-        suite.addTest(unittest.makeSuite(TestSomeProduct))
+        suite.addTest(unittest.makeSuite(TestCustomProperties))
         return suite
